3b68f7c509e5607aa078cc16435c89cc4e625c2f
jjconti <jjconti@07477463-646b-0410-87cb-c49d6d69b0f2>
2010-06-18 14:26:50 +0000

    little fixes

--- wrapstrings/wrapstrings.py
+++ wrapstrings/wrapstrings.py
@@ -1,8 +1,15 @@
+'''
+Author: Juan Jose Conti <jjconti@gmail.com>
+
+This module can parse Python files looking for uses of the % operator.
+The % operator is mainly used for string formatting operations.
+
+Use:
+
+    python wrapstrings.py file.py funcName
+    
+'''
 import ast
-
-def myfunc(a):
-    return a
-
 
 class RewriteMod(ast.NodeTransformer):
 
@@ -13,15 +20,25 @@ class RewriteMod(ast.NodeTransformer):
         Si node.op es instancia de Mod y node.left es instancia de Str,
         tomar node.left y cambiarlo por una llamada a funcion.
         '''
-        if isinstance(node.op, ast.Mod) and isinstance(node.left, ast.Str):
-                node.left = ast.Call(func=ast.Name(id="myFunc"), args=[node.left])
+        if isinstance(node.op, ast.Mod) and (changeall or isinstance(node.left, ast.Str)):
+                node.left = ast.Call(func=ast.Name(id=funcname), args=[node.left])
                 return node
         return node
 
-a = ast.parse(open('ejemplo.py').read())
 
-RewriteMod().visit(a)
+if __name__ == '__main__':
+    import sys
+    filename = sys.argv[1]
+    funcname = sys.argv[2]
+    if len(sys.argv) > 3:
+        changeall = True
+    else:
+        changeall = False
+        
+    a = ast.parse(open(filename).read())
+    
+    RewriteMod().visit(a)
 
-import codegen
-print codegen.to_source(a)
+    import codegen
+    print codegen.to_source(a)
 
